63ed869c9b030e79f0559e51fa34d12990bea1bb
iamtakagi <github@iamtakagi.net>
2025-07-30 15:53:42 +0900

    fix: for all linux distributions

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,7 +1,3 @@
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-
-sudo apt-get update
-sudo apt-get install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
 
 sudo tailscale up
